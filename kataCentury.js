92f9f7b57cafd0c46ec7307d6be01c00435e2659
gary <gary.white@lodgit.com>
2021-09-21 16:41:40 +0200

    code, sort of working

--- kataCentury.js
+++ kataCentury.js
@@ -1,11 +1,16 @@
 function century(year) {
-    // Finish this :)
     var ans = (''+year).split('');;
     if (ans.length <= 2) {
-        // return ans[0];
         return 1;
     } else if (ans.length == 3){
         return ans[0];
+    } else if (ans.length >= 4 && ans[ans.length - 1] == 0){
+        ans = ans[0] + ans[1];
+        return parseInt(ans, 10);
+    } else if (ans.length >= 4 && ans[ans.length - 1] >= 1){
+        ans = ans[0] + ans[1];
+        ans = parseInt(ans, 10) +1;
+        return ans;
     }
 }
 
@@ -16,3 +21,6 @@ console.log(century(201)); // 2
 console.log(century(300)); // 3
 console.log(century(441)); // 4
 console.log(century(571)); // 5
+console.log(century(1900)); // 19
+console.log(century(1901)); // 20
+console.log(century(123924)); // 1240
